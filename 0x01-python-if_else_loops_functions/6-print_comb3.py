a6af27c3666910c43ede861f19ef0162fb09e25e
root <root@Amir.localdomain>
2023-08-08 20:44:54 +0300

    first commit from vscode

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for out in range(10):
-    for inr in range(out + 1, 10):
-        if out == inr:
-            continue
-        if inr == 9 and out == 8:
-            print("{:d}{:d}".format(out, inr))
-            break
-        print("{:d}{:d}".format(out, inr), end=", ")
+	for inr in range(out + 1, 10):
+		if out == inr:
+			continue
+		if inr == 9 and out == 8:
+			print("{:d}{:d}".format(out, inr))
+			break
+		print("{:d}{:d}".format(out, inr), end=", ")
