0ef5ce4801a7508387594525a3172133d015ad35
root <root@Amir.localdomain>
2023-08-08 21:23:32 +0300

    second commit fromo vscode, the life is so easy

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    return (abs(number) % 10)
+def uppercase(str):
+    res = ""
+    for i in str:
+        if ord(i) <= 97 or ord(i) >= 122:
+            res += chr(ord(i) - 32)
+        else:
+            res += i
+    return res
