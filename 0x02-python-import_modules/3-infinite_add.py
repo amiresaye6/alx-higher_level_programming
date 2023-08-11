708f6eb2b81e9201bd4419acddceddbd9f346e76
Amir Alsayed <root@Amir.localdomain>
2023-08-11 12:56:06 +0300

    debugging

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     sum = 0
-    for i, arg in enumerate(sys.argv[1:], start= 1):
+    for i, arg in enumerate(sys.argv[1:], start=1):
         sum += int(arg)
     print("{}".format(sum))
