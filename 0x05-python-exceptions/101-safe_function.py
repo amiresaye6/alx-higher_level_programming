7dbea39d082988526ebeec3fed73ece451c4bf8d
Amir Alsayed <amyralsyd367@gmail.com>
2023-08-28 17:59:50 +0300

    DEBUGING

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,26 +5,3 @@ def safe_function(fct, *args):
         return res
     except Exception as err:
         print("Exception: {}".format(err))
-
-
-
-
-def my_div(a, b):
-    return a / b
-
-result = safe_function(my_div, 10, 2)
-print("result of my_div: {}".format(result))
-
-result = safe_function(my_div, 10, 0)
-print("result of my_div: {}".format(result))
-
-
-def print_list(my_list, len):
-    i = 0
-    while i < len:
-        print(my_list[i])
-        i += 1
-    return len
-
-result = safe_function(print_list, [1, 2, 3, 4], 10)
-print("result of print_list: {}".format(result))
--- 0x05-python-exceptions/git_life.sh
+++ 0x05-python-exceptions/git_life.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "finishing 150% of the task"
+git commit -m "DEBUGING"
 git push
