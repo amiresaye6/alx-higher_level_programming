2e298ecbd7353b5e8a7b4a80fd91f4829a82f6f7
Amir Alsayed <amyralsyd367@gmail.com>
2023-08-28 18:17:37 +0300

    DEBUGING

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_function(fct, *args):
     try:
         res = fct(*args)
         return res
     except Exception as err:
-        print("Exception: {}".format(err))
+        print("Exception: {}".format(err), file=sys.stderr)
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,15 +3,17 @@ def list_division(my_list_1, my_list_2, list_length):
     res = [0] * list_length
     for index in range(list_length):
         try:
-            res[index] = my_list_1[index] / my_list_2[index]
+            temp = my_list_1[index] / my_list_2[index]
         except IndexError:
-            res[index] = 0
+            temp = 0
             print("out of range")
         except ZeroDivisionError:
-            res[index] = 0
+            temp = 0
             print("division by 0")
-        except TypeError:
-            res[index] = 0
+        except ValueError:
+            temp = 0
             print("wrong type")
+        finally:
+            res[index] = temp
 
     return res
