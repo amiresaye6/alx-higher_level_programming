ba82b45c38f480cb764f50474e4f765640cc8ca9
Amir Alsayed <amyralsyd367@gmail.com>
2023-09-13 01:07:21 +0300

    half the way :)

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,8 @@
 
 
 from sys import argv
-load_from_json = __import__("6-load_from_json_file.py").load_from_json_file
-save_to_json = __import__("5-save_to_json_file.py").save_to_json_file
+load_from_json = __import__("6-load_from_json_file").load_from_json_file
+save_to_json = __import__("5-save_to_json_file").save_to_json_file
 
 
 fileName = "add_item.json"
