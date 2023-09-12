9b73b5a40b5d0fbc8c7d104831bcd132e90976fd
Amir Alsayed <amyralsyd367@gmail.com>
2023-09-13 01:03:57 +0300

    half the way :)

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""file input / output"""
+import json
+
+
+def load_from_json_file(filename):
+    """ function that creates an Object from a “JSON file”:"""
+
+    with open(filename, 'r', encoding="utf-8") as file:
+        return json.load(file)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""file input /output"""
+
+
+from sys import argv
+load_from_json = __import__("6-load_from_json_file.py").load_from_json_file
+save_to_json = __import__("5-save_to_json_file.py").save_to_json_file
+
+
+fileName = "add_item.json"
+
+try:
+    """if the file exists"""
+    json_list = load_from_json(fileName)
+except FileNotFoundError:
+    json_list = []
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json(json_list, fileName)
