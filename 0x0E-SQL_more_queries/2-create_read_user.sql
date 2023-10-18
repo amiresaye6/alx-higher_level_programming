9706405dc6f364214d800c98dabf82a76aed91f2
Amir Alsayed <amyralsyd367@gmail.com>
2023-10-19 00:39:52 +0300

    oh NOOOO

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,7 @@
--- a script that creates the MySQL server user user_0d_1.
-CREAT USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- a script that creates the database hbtn_0d_2 and the user user_0d_2
+-- creat database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creat user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- giving grant privlages to user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
