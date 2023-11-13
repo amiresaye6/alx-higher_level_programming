618a4c9628f55621a071d593db54fc4adc7a7162
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-13 15:51:18 +0200

    debug

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const args = process.argv.slice(2);
 let number = parseInt(args[0]);
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 function add (a, b) {
   return (a + b);
