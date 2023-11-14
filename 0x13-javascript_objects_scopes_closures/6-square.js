6a476ab1ea8afd1b2c945c0c2d26ca96d272a1b3
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-14 15:28:53 +0200

    debug and style

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,20 +3,13 @@
 const Square_old = require('./5-square');
 
 class Square extends Square_old {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
-    let sign;
-    if (typeof (c) === undefined) {
-      sign = c;
-    } else {
-      sign = 'X';
+    if (c === undefined) {
+      c = 'X';
     }
     let line = '';
     for (let i = 0; i < this.width; i++) {
-      line += sign;
+      line += c;
     }
     for (let j = 0; j < this.height; j++) {
       console.log(line);
