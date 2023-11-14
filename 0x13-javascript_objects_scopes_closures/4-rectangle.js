e588111943a73eb4f4e449609a28527f0a771ff8
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-14 15:09:18 +0200

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const sign = 'X';
+    let line = '';
+    for (let i = 0; i < this.width; i++) {
+      line += sign;
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
