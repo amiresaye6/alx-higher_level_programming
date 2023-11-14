3508972f59bbd0e8b776fc31961748ab0b9b99a0
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-14 15:40:46 +0200

    debug and style

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 exports.esrever = function (list) {
   const newList = [];
   const len = list.length;
-  for (let i = 0; i < len; list++) {
+  for (let i = 0; i < len; i++) {
     newList[len - i - 1] = list[i];
   }
   return (newList);
