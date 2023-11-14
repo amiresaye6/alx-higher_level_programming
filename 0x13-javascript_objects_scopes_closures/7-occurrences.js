6a2398550ff25b476dad60a2f783a7e791bbb778
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-14 15:33:24 +0200

    debug and style

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let ocurances = 0;
+  let i = 0;
+  if (list != undefined && searchElement != undefined) {
+    while (list[i] != undefined) {
+      if (list[i] === searchElement) {
+        ocurances++;
+      }
+      i++;
+    }
+  }
+  return (ocurances);
+};
