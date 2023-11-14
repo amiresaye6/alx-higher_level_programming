708fdf06c2ca0aa7d2fe37b2c9ddacced82d7298
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-14 14:41:38 +0200

    inint commit

--- 0x13-javascript_objects_scopes_closures/go_life.sh
+++ 0x13-javascript_objects_scopes_closures/go_life.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-// change mode to executable
+# // change mode to executable
 chmod +x *.js
 
-// checkign the styling of the code
-semistandard *.js
+# // checkign the styling of the code
+semistandard --fix *.js
 
-// adding, commiting, and pushing to github.
+# // adding, commiting, and pushing to github.
 git add .
 git commit -m "inint commit"
 git push
