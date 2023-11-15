310748973d60e106ae5b8770ade970a6606253aa
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-15 13:56:06 +0200

    debug and style

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,16 +1,5 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
-    }
-  }
 
-  print () {
-    console.log('hi there');
-  }
 }
 module.exports = Rectangle;
-const rec = new Rectangle(2, 2);
-rec.print();
