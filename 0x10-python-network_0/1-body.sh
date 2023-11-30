82b2c2a4e52f10a0492ece575f19da6465e6ec30
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-30 22:20:57 +0200

    debug

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the body
-# of the response
-curl -s -L $1 
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
