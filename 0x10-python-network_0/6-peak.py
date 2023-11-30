4f753638b2da1e42f5a70bc7aecd09a46299612e
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-30 23:21:55 +0200

    style :P

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,37 +6,30 @@ unsorted integers.
 
 
 def find_peak(list_of_integers):
-	"""
-	a function that finds a peak in a list of unsorted integers.
-	"""
-	if (list_of_integers is None or len(list_of_integers) < 1):
-		return None
+    """
+    a function that finds a peak in a list of unsorted integers.
+    """
+    if (list_of_integers is None or len(list_of_integers) < 1):
+        return None
 
-	length = len(list_of_integers)
-	start = 0
-	mid = int(((length - start) // 2) + start)
+    length = len(list_of_integers)
+    start = 0
+    mid = int(((length - start) // 2) + start)
 
-	if (length == 1):
-		return (list_of_integers[0])
-	if (length == 2):
-		return max(list_of_integers)
-	
-	if (list_of_integers[0] > list_of_integers[1] and\
-	 	list_of_integers[0] >= 0):
-		return list_of_integers[0]
+    if (length == 1):
+        return (list_of_integers[0])
+    if (length == 2):
+        return max(list_of_integers)
 
-	if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-			list_of_integers[mid] >= list_of_integers[mid + 1]:
-		return list_of_integers[mid]
+    if (list_of_integers[0] > list_of_integers[1] and
+            list_of_integers[0] >= 0):
+        return list_of_integers[0]
 
-	if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-		return find_peak(list_of_integers[mid:])
-	if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-		return find_peak(list_of_integers[:mid])
+    if (list_of_integers[mid] >= list_of_integers[mid - 1] and
+            list_of_integers[mid] >= list_of_integers[mid + 1]):
+        return list_of_integers[mid]
 
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
-print(find_peak([-2, -4, 2, 1]))
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
